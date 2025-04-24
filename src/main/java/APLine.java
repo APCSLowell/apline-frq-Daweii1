500b5d420035bd6c84ea1a0801d9af90e42ce4cb
Dawei <107432606+Daweii1@users.noreply.github.com>
2025-04-24 11:30:36 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,5 +1,29 @@
 public class APLine
 {
-  /* your code here */
+  private double a,b,c;
+
+  public APLine(int x,int y,int z) {
+    a = x;
+    b = y;
+    z = c;
+
+  }
+
+  public double getSlope() {
+  double boom = (-a)/b;
+    return boom;
+
+
+  }
+
+  public boolean isOnLine(int xCord, int yCord) {
+  if(yCord == (((-a*xCord)-c)/b))
+    return true;
+
+    else
+    return false;
+
+  }
+
   
 }
